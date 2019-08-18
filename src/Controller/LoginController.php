3ba5ff73ce58035d36504215a9982cc9705450b2
Miguel Ramirez <themike123me@gmail.com>
2019-08-18 17:13:05 -0500

    Creación del form login y proceso en el controlador

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,17 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
+        $error = $authenticationUtils->getLastAuthenticationError(); 
+	    $lastUsername = $authenticationUtils->getLastUsername();
+                                                                                                            
+        return $this->render('login/index.html.twig', ['last_username' => $lastUsername, 'error' => 	$error]);
+        
     }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    
+    }
+
 }
