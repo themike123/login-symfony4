9ce5c1e84f32f55e8ed750d4f35483860853016b
Miguel Ramirez <themike123me@gmail.com>
2019-08-18 17:30:25 -0500

    Configuración del login y se cargo el usuario a la base de datos

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setPassword(
+            $this->encoder->encodePassword($user, 'admin')
+        );
 
+        $user->setRole('ROLE_ADMIN');
+        $user->setEmail('themike123me@gmail.com');
+        $manager->persist($user);
         $manager->flush();
     }
 }
